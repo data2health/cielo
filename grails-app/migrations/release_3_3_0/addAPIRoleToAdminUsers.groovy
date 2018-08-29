504e8b409304d52e8443d9afecfef7bb39033e65
Ricardo Rodriguez <ricardo.rodriguez@wustl.edu>
2018-08-28 20:40:13 -0500

    Fix for failing migration that caused app to fail to load; bumped version to 3.3.1

--- grails-app/migrations/release_3_3_0/addAPIRoleToAdminUsers.groovy
+++ grails-app/migrations/release_3_3_0/addAPIRoleToAdminUsers.groovy
@@ -9,13 +9,12 @@ databaseChangeLog = {
 
     changeSet(author: "rickyrodriguez (manual)", id: "1535117228830-1") {
         grailsChange {
-            UserRole apiRole = new UserRole(authority: UserRolesEnum.ROLE_API.name()).save(flush: true)
-            AclSid aclSid       = new AclSid()
-            aclSid.principal    = false
-            aclSid.sid          = UserRolesEnum.ROLE_API.name()
-            aclSid.save(flush: true)
-
             change {
+                UserRole apiRole = new UserRole(authority: UserRolesEnum.ROLE_API.name()).save(flush: true)
+                AclSid aclSid       = new AclSid()
+                aclSid.principal    = false
+                aclSid.sid          = UserRolesEnum.ROLE_API.name()
+                aclSid.save(flush: true)
                 UserAccountUserRole.findAllByUserRole(UserRole.findByAuthority(UserRolesEnum.ROLE_SUPERUSER.name()))?.each { UserAccountUserRole userAccountUserRole ->
 
                     if (apiRole) UserAccountUserRole.create(userAccountUserRole.userAccount, apiRole).save()
